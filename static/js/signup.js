a70ffaaae01f3753175790bbb236714c44fc1013
MuxLee <dnfl7080@Naver.com>
2018-12-13 14:39:42 +0900

    .

--- static/js/signup.js
+++ static/js/signup.js
@@ -8,8 +8,16 @@ $(document).ready(function () {
     $('#signup').css('height', getSignupH);
     $('#signup').fadeIn(600);
     $(window).resize(function () {
+        var getSignWindowW = $(window).width();
+        var getSignWindowH = $(window).height();
+        var getSignupW = $('#signup').width();
         var getSignupH = $('#signup').height();
+        $('#signup').css('left', (getSignWindowW / 2) - (getSignupW / 2));
+        $('#signup').css('top', (getSignWindowH / 2) - (getSignupH / 2));
         $('#signup').css('height', getSignupH);
+        if (getSignWindowW <= 750) {
+            $('#signup').fadeIn(600);
+        }
     });
     var getFormDivW = $('#signup_form_div').width();
     var getLabelFieldDivW = $('.label_field_div').width();
