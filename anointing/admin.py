b974c997df052f128596739d951ba2f2b53e1800
MuxLee <dnfl7080@daum.net>
2019-01-04 11:42:41 +0900

    ...

--- anointing/admin.py
+++ anointing/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from anointing.models import Post
 
-class AnoitingPostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'modify_date')
+class AnointingPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'modify_date', )
     list_filter = ('modify_date', )
     search_fields = ('title', 'content', 'author', )
     prepopulated_fields = {'slug': ('title', )}
 
-admin.site.register(Post, AnoitingPostAdmin)
+admin.site.register(Post, AnointingPostAdmin)
